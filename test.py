09bbeff1a520908c9956793dbbac801fc385a960
kalas <o010oo78rus@gmail.com>
2023-11-20 14:28:24 +0300

    Сортировка успешна 'th' 'a'

--- test.py
+++ test.py
@@ -1,23 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
-import lxml
-import pandas as pd
+import re
 
-
-# Create an URL object
 url = 'https://en.wikipedia.org/wiki/Programming_languages_used_in_most_popular_websites'
 
-# Create object page
 page = requests.get(url)
 print(page)
 
-# parser-lxml = Change html to Python friendly format
-# Obtain page's information
-soup = BeautifulSoup(page.text, 'lxml')
+soup = BeautifulSoup(page.text, "lxml")
 
-
-#a = soup.find('div', class_='wm_parser_output').find_all('tr')
-#result = {}
-
+a = soup.findAll({'th' : True, 'a' :True})
 print(a)
+
+
+
+
+
+
 
