f1eb370f97f94d337b5d6093ba1909d954ee4470
rmn178 <o01078rus@gmail.com>
2023-11-28 11:29:34 +0300

    123

--- test.py
+++ test.py
@@ -1,18 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
-import re
 
 url = 'https://en.wikipedia.org/wiki/Programming_languages_used_in_most_popular_websites'
 
 page = requests.get(url)
-print(page)
 
 soup = BeautifulSoup(page.text, "lxml")
 
-a = soup.findAll({'th' : True, 'a' :True})
-print(a)
+# Найдем таблицу с данными
+table = soup.find('table', {'class': 'wikitable'})
 
+# Итерируемся по строкам таблицы
+for row in table.find_all('tr')[1:]:  # Пропускаем заголовок
+    columns = row.find_all('td')
+    if len(columns) == 0:
+        continue  # Пропустим строки заголовка и другие пустые строки
 
+#website =
+#popularity =
+#frontend =
+#backend =
+#database =
+#notes =
+52
 
 
 
